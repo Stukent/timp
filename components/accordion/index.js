c2aff14411792f1d2aa505c5896d99dd97addf5b
Romina Painter <romyharley@hotmail.com>
2018-01-18 17:08:55 -0700

    Make the accordion collapse.

--- components/accordion/index.js
+++ components/accordion/index.js
@@ -1,43 +1,33 @@
-import React from 'react'
+import React, { Component } from 'react'
 import './style.scss'
 
 
-const Accordion = () => {
-  function handleClick(e) {
-    e.preventDefault()
-    console.log('The link was clicked.') // eslint-disable-line no-console
+class Accordion extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      open: true,
+    }
+    this.handleClick = this.handleClick.bind(this)
   }
-
-  return (
-    <div className="accordion-wrapper">
-      <button className="accordion" onClick={handleClick}> Section 1 </button>
-      <div className="panel">
-        <p>
+  handleClick(e) {
+    e.preventDefault()
+    this.setState({ open: !this.state.open })
+  }
+  render() {
+    return (
+      <div className="accordion-wrapper">
+        <button className="accordion" onClick={this.handleClick}> Section 1 </button>
+        <div className={`panel ${this.state.open ? '' : 'panel-hidden'}`}>
+          <p>
           Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
           incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
            exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-        </p>
+          </p>
+        </div>
       </div>
-
-      <button className="accordion">Section 2</button>
-      <div className="panel">
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
-          incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-           exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-        </p>
-      </div>
-
-      <button className="accordion">Section 3</button>
-      <div className="panel">
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
-          incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-           exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-        </p>
-      </div>
-    </div>
-  )
+    )
+  }
 }
 
 export default Accordion
