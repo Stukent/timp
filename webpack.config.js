fabd50808fd1f4abdf7b2fb43b1ff084b08cd104
Hilton Shumway <hillshum@gmail.com>
2018-02-15 16:02:36 -0700

    Fix eslint warnings in webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 // Example webpack configuration with asset fingerprinting in production.
-"use strict";
+
 
 // See for reference:
 // http://tech.trivago.com/2015/12/17/export-multiple-javascript-module-formats/
@@ -9,42 +9,42 @@ function createConfig(target) {
     entry: './index.js',
 
     output: {
-      path: __dirname +'/dist/',
-      filename: 'timp.' + target + '.js',
+      path: `${__dirname}/dist/`,
+      filename: `timp.${target}.js`,
       library: 'timp',
-      libraryTarget: target
+      libraryTarget: target,
     },
 
     module: {
       loaders: [
-          { 
-            loader: "babel-loader",
-            test: /\.js$/,
-          },
-          {
-            test: /\.scss$/,
-            loaders: ["style-loader", "css-loader", "sass-loader"]
-          },
-          {
-            test: /\.inline.svg$/,
-            loader: 'babel!svg-react',
-          },
-          {
-            test: /\.jpe?g$|\.gif$|\.png$|^(?!.*\.inline\.svg$).*\.svg$/,
-            loader: 'url',
-          },
-        ]
+        {
+          loader: 'babel-loader',
+          test: /\.js$/,
+        },
+        {
+          test: /\.scss$/,
+          loaders: ['style-loader', 'css-loader', 'sass-loader'],
+        },
+        {
+          test: /\.inline.svg$/,
+          loader: 'babel!svg-react',
+        },
+        {
+          test: /\.jpe?g$|\.gif$|\.png$|^(?!.*\.inline\.svg$).*\.svg$/,
+          loader: 'url',
+        },
+      ],
     },
 
     resolve: {
-      root: './'
+      root: './',
     },
-  };
+  }
 }
 
 module.exports = [
   createConfig('var'),
   createConfig('commonjs2'),
   createConfig('amd'),
-  createConfig('umd')
-];
+  createConfig('umd'),
+]
