5f1b20e68d7df6867c40b1f38b97ffaa2dfe3f18
Colin Key <colin.m.key@gmail.com>
2019-01-08 13:30:11 -0700

    Add addons for readme, props (knobs), and a11y

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,4 +1,9 @@
-import { configure } from '@storybook/react'
+import { configure, addDecorator } from '@storybook/react'
+import { checkA11y } from '@storybook/addon-a11y'
+import { withKnobs } from '@storybook/addon-knobs'
+
+addDecorator(checkA11y)
+addDecorator(withKnobs)
 
 function loadStories() {
   require('../stories/index.js')
